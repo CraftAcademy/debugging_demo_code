40708b8ddf379ec9c64e08cae3ac68b7e7c85d7a
Thomas <thomas@craftacademy.se>
2019-07-04 10:34:40 +0200

    moves out js to separate file changes title of page modifies content of h1
    Co-authored-by: Nicklas <nicklas.rohman@jpj.com>

---
+++ app.js
@@ -0,0 +1,30 @@
+const inputsAreEmpty = () => {
+  if (isNaN(getNumber(1)) || isNaN(getNumber(2))) {
+    return true;
+  } else {
+    return false;
+  }
+}
+const getNumber = index => {
+  return parseFloat(inputs[index - 1].value)
+}
+
+const updateLabel = () => {
+  let number1 = getNumber(1);
+  let number2 = getNumber(2);
+  var sum = number1 + number2;
+  label.textContent = number1 + ' + ' + number2 + ' = ' + sum;
+}
+
+const onButtonClick = () => {
+  if (inputsAreEmpty()) {
+    label.textContent = 'Error: one or both inputs are empty.';
+    return;
+  }
+  updateLabel();
+}
+
+let label = document.getElementById('results');
+let inputs = document.querySelectorAll('input');
+let button = document.querySelector('button');
+button.addEventListener('click', onButtonClick);
